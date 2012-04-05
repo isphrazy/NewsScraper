df429d0234b5656f6fd9b62a6661e903599ca345
Pingyang He <pingyh@cs.washington.edu>
2012-04-04 22:37:37 -0700

    sample parse done

--- src/main/java/edu/washington/cs/NewsScraper/NewsScraperMain.java
+++ src/main/java/edu/washington/cs/NewsScraper/NewsScraperMain.java
@@ -4,16 +4,18 @@ import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 public class NewsScraperMain 
 {
     public static void main( String[] args ) throws IOException
     {
-//        System.out.println( "Hello World!" );
-    	String url = "http://www.cnn.com/2012/04/04/showbiz/whitney-houston-toxicology/index.html";
+    	String url = "http://www.cnn.com/2012/04/04/showbiz/whitney-houston-toxicology/index.html?eref=rss_topstories&utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+rss%2Fcnn_topstories+%28RSS%3A+Top+Stories%29";
         Document doc = Jsoup.connect(url).get();
-        System.out.println(doc.title());
+        Elements content = doc.select("p.cnn_storypgraph2");
+        System.out.println(content.text());
     }
    
 }
